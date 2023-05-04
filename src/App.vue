6e08c86e3426b15cc8fedf9c9f8cfbb55626211f
xiaoyan <3072861626@qq.com>
2023-05-04 22:08:07 +0800

    refactor: add home page

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import ToggleTheme from './components/ToggleTheme.vue';
+</script>
+
+<template>
+  <header>
+    <h1 class="title">DOT</h1>
+    <p class="summay">Welcome to my repository for my personal hobby projects!</p>
+    <ToggleTheme class="right-top"></ToggleTheme>
+  </header>
+
+</template>
+
+<style>
+  .right-top {
+    position: absolute;
+    top: 24px; right: 24px;
+  }
+</style>
