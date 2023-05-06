063f9ec8298573b1561e75a1865f042e4f0e2c70
xiaoyan <3072861626@qq.com>
2023-05-06 20:58:09 +0800

    Add project list

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,72 @@
 <script setup>
-import ToggleTheme from './components/ToggleTheme.vue';
+import ToggleTheme from './components/ToggleTheme.vue'
 </script>
 
 <template>
   <header>
     <h1 class="title">DOT</h1>
-    <p class="summay">Welcome to my repository for my personal hobby projects!</p>
+    <p class="summay">
+      Welcome to my repository for my personal hobby projects!
+    </p>
     <ToggleTheme class="right-top"></ToggleTheme>
   </header>
-
+  <main>
+    <ul class="list">
+      <li class="list-item">
+        <img class="night" src="/public/cover/banner.png" alt="cover" />
+        <a class="list-title" href="./package/banner/">Banner</a>
+      </li>
+      <li class="list-item">
+        <img class="night" src="/public/cover/lightTip.png" alt="cover" />
+        <a class="list-title" href="./package/LightTip/">lightTip</a>
+      </li>
+    </ul>
+  </main>
 </template>
 
-<style>
-  .right-top {
-    position: absolute;
-    top: 24px; right: 24px;
+<style lang="scss">
+.right-top {
+  position: absolute;
+  top: 24px;
+  right: 24px;
+}
+
+.list {
+  display: grid;
+  grid-gap: 1rem;
+  gap: 1rem;
+  grid-template-columns: repeat(1, minmax(0, 1fr));
+  padding: 0;
+  margin-bottom: 24px;
+
+  .list-item {
+    display: flex;
+    flex-direction: column;
+    padding: 16px;
+    border-radius: 8px;
+    background-color: var(--bg-card-color);
+    transition: background-color 0.3s;
+
+    img {
+      width: 100%;
+      height: 200px;
+      border-radius: 8px;
+    }
+
+    .list-title {
+      margin: 12px 0;
+      font-weight: 600;
+      transition: color .3s;
+      list-style: square;
+
+      &:hover { color: var(--primary); }
+    }
   }
+}
+
+@media (min-width: 640px) {
+  .list {
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+  }
+}
 </style>
